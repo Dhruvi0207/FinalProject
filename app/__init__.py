b9d5e926b6d8e7af79368f0a775374b83fa41085
Dhruvi0207 <dhruvi.kapadia1@dcmail.ca>
2024-08-09 19:27:24 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin@localhost/final_project'
+
+    # Use environment variable for database URI in production
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'mysql+pymysql://root:admin@localhost/final_project')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Optional: to disable Flask-SQLAlchemy event system
+
     db.init_app(app)
 
     with app.app_context():
